5e2a0ea8d21911b18fb5543d5ec56fe6d2c0ddab
Karthikeyan-Arunachalam-6285 <78998503+Karthikeyan-Arunachalam-6285@users.noreply.github.com>
2022-09-20 20:49:35 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import Print from './component/print';
+import logo from "./logo.svg";
+import "./App.css";
+import Print from "./component/print";
 import { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
-import { Button,Container } from 'react-bootstrap';
+import { Button, Container } from "react-bootstrap";
 
 const App = () => {
   const componentRef = useRef();
   const handlePrint = useReactToPrint({
-    content: () => componentRef.current
+    content: () => componentRef.current,
   });
 
   return (
     <div className="bg-gray-200 p-6">
-            <Print ref={componentRef} />
-
-            <Container >
-
-      <Button
-        type="button"
-        className="bg-gray-500 border border-gray-500 p-2 mb-4"
-        onClick={handlePrint}
-      >
-        {" "}
-        Print {" "}
-      </Button>
+      <Print ref={componentRef} />
+      <Container>
+        <Button
+          type="button"
+          className="bg-gray-500 border border-gray-500 p-2 mb-4"
+          onClick={handlePrint}
+        >
+          {" "}
+          Print{" "}
+        </Button>
       </Container>{" "}
-
-      </div>
-   );
+    </div>
+  );
 };
 
 export default App;
--- src/component/print.js
+++ src/component/print.js
@@ -3,21 +3,21 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ReactToPrint from "react-to-print";
 // import { ComponentToPrint } from './ComponentToPrint';
-import React,{Component} from "react";
+import React, { Component, useState } from "react";
 import { useRef } from "react";
-
 const Print = React.forwardRef((props, ref) => {
-     
-  
-    return (
-      <div className="bg-gray-200 p-6" ref={ref}>
-  <Container style={{ padding: "10px" }}>
+const [name, setName] = useState("")
+
+  return (
+    <div className="bg-gray-200 p-6" >
+      <Container style={{ padding: "10px" }}>
         <Row>
-            <Form.Control as="textarea" placeholder="Leave a comment here" />
-         </Row>{" "}
+          <Form.Control as="textarea" placeholder="Leave a comment here" onChange={e=>setName(e.target.value)}  style={{ width: "500px" }}/>
+          <p ref={ref}>{name}</p>
+        </Row>{" "}
         <div style={{ padding: "10px", float: "right" }}></div>
       </Container>{" "}
-      </div>
-    );
-  });
+    </div>
+  );
+});
 export default Print;
